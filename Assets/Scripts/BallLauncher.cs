83cbf89282674f0f44f48557c46a2c893e773cb2
niskanjo <niskanjo@arcada.fi>
2022-12-08 16:05:36 +0200

    Add functional Launcher and block assets

--- Assets/Scripts/BallLauncher.cs
+++ Assets/Scripts/BallLauncher.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallLauncher : MonoBehaviour
 {
-    public Vector2 Force;
     public Vector2 StartPosition;
     public Vector2 EndPosition;
+    public Vector2 Direction;
     Rigidbody2D Physics;
+    float force = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +21,28 @@ public class BallLauncher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Physics.AddForce(Force);
         if (Input.GetMouseButtonDown(0) == true)
         {
-            StartPosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-            StartPosition.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
-            Debug.Log("Left mouse button pressed at x = " + StartPosition.x + ", y = " + StartPosition.y);
+            StartPosition = Input.mousePosition;
+            Debug.Log("Left mouse button pressed at " + StartPosition);
 
         }
         if (Input.GetMouseButtonUp(0) == true)
         {
-            EndPosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-            EndPosition.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
-            Debug.Log("Left mouse button released at x = " + EndPosition.x + ", y = " + EndPosition.y);
+            EndPosition = Input.mousePosition;
+            Debug.Log("Left mouse button released at " + EndPosition);
+            Direction = (StartPosition - EndPosition);
+            Physics.AddForce(Direction * force);
 
         }
 
-        Force = (EndPosition - StartPosition) * -1;
-        Debug.Log("Force: " + Force);
+        
+        Debug.Log("Force: " + force);
 
+        if (transform.position.x < -10 || transform.position.x > 10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
 }
